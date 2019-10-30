a2e5529b6b84ee3a48b7680362f5ad848da10460
Geo <sdi1200192@di.uoa.gr>
2019-10-31 00:29:12 +0200

    Changes have been made so there's less memory allocation and likely speed has also improved due to certain actions being uneeded now

--- relation.c
+++ relation.c
@@ -4,53 +4,7 @@
 #define MAXS 65536
 
 
-int radix_sort(relation *rel,int depth){	//call it with depth argument value 0, rel will point at a sorted relation at the end.
-	uint64_t hist[256];
-	create_hist(rel,hist,depth);
-	uint64_t psum[256]; 
-	create_psum(hist,psum);
-	relation *rel2;
-	rel2 = (relation*) malloc(sizeof (relation));
-	rel2->tuples = (tuple*)malloc(sizeof(tuple)*rel->num_tuples);
-	rel2->num_tuples = rel->num_tuples;
-	uint64_t i;
-	unsigned int j;
-	for (i=0;i<rel->num_tuples;i++){ 	//copies the contents of rel to rel2 partialy sorted
-		j = n_first_bytes_num (rel->tuples[i],depth);
-		rel2->tuples[psum[j]] = rel->tuples[i];
-		psum[j]++;
-	}
-	create_psum(hist,psum);
-	for (j=0;j<256;j++){
-		if(hist[j] !=0){
-			if(hist[j]*16< MAXS || depth == 8){	//checks if the amount of memory the tuples occupy in bucket j is less than 64kb
-				isolate(rel2,psum[j],hist[j],rel);	//rel will have only the contents of bucket j
-				quicksort (rel,0,(rel->num_tuples-1)); //rel gets sorted
-				copy_relation (rel2,psum[j],hist[j],rel); //the sorted result is then copied back to rel2 in the corret place
-			}
-			else{ //in case the memory the tuples occupy in bucket j is more or equal to 64kb
-				isolate(rel2,psum[j],hist[j],rel); //rel will have only the contents of bucket j
-				radix_sort(rel,(depth+1)); //radix_sort only on rel that now has only the contents of bucket j and depth is incr by 1
-				copy_relation (rel2,psum[j],hist[j],rel); //the sorted result is then copied back to rel2 in the correct place
-			}	
-		}
-	}
-	rel->num_tuples = rel2->num_tuples; //finally rel becomes a sorted.
-	copy_relation(rel,0,rel2->num_tuples,rel2);
-	free(rel2->tuples);
-	free(rel2);
-	return 0;
-}
-
-/*int call_radix(relation *rel){
-	
-	rel2 = (relation*)malloc(sizeof (relation));
-	rel2->tuples = (tuple*)malloc(sizeof(tuple)*rel->num_tuples);
-	rel->num_tuples = rel->num_tuples;
-
-}
-
-int radix_sort(relation *rel,relation *rel2, int depth){	//call it with depth argument value 0, rel will point at a sorted relation at the end.
+/*int radix_sort(relation *rel,int depth){	//call it with depth argument value 0, rel will point at a sorted relation at the end.
 	uint64_t hist[256];
 	create_hist(rel,hist,depth);
 	uint64_t psum[256]; 
@@ -87,6 +41,69 @@ int radix_sort(relation *rel,relation *rel2, int depth){	//call it with depth ar
 	free(rel2);
 	return 0;
 }*/
+
+int sort(relation *rel){
+	if (rel->num_tuples*16 < MAXS){//if the relation is small on it's own jump straight into qucksort
+		quicksort (rel,0,(rel->num_tuples-1));
+		return 0;
+	}
+	relation *rel2;
+	rel2 = (relation*)malloc(sizeof (relation));
+	rel2->tuples = (tuple*)malloc(sizeof(tuple)*rel->num_tuples);
+	rel2->num_tuples = rel->num_tuples;
+	radix_sort(rel,rel2,0,0);
+	rel->num_tuples = rel2->num_tuples; 
+	copy_relation(rel,0,rel2->num_tuples,rel2);
+	free(rel2->tuples);
+	free(rel2);
+	return 0;
+}
+
+int radix_sort(relation *rel,relation *rel2, int depth,uint64_t start){	//call it with depth argument value 0, rel will point at a sorted relation at the end.
+	printf("hi\n");
+	uint64_t hist[256];
+	create_hist(rel,hist,depth);
+	uint64_t psum[256]; 
+	create_psum(hist,psum,start);
+	uint64_t i;
+	unsigned int j;
+	printf("gate 0\n");
+	for (i=0;i<rel->num_tuples;i++){ 	//copies the contents of rel to rel2 partialy sorted
+		j = n_first_bytes_num (rel->tuples[i],depth);
+		rel2->tuples[psum[j]] = rel->tuples[i];
+		psum[j]++;
+	}
+	printf("gate 1\n");
+	create_psum(hist,psum,start);
+	for (j=0;j<256;j++){
+		if(hist[j] !=0){
+			if(hist[j]*16< MAXS || depth == 8){	//checks if the amount of memory the tuples occupy in bucket j is less than 64kb
+				printf("gate 2\n");
+				isolate(rel2,psum[j],hist[j],rel);	//rel will have only the contents of bucket j
+				printf("gate 3\n");
+				quicksort (rel,0,(rel->num_tuples-1)); //rel gets sorted
+				printf("gate 4\n");
+				copy_relation (rel2,psum[j],hist[j],rel); //the sorted result is then copied back to rel2 in the corret place
+				printf("gate 5\n");
+			}
+			else{ //in case the memory the tuples occupy in bucket j is more or equal to 64kb
+				printf("gate 6\n");
+				isolate(rel2,psum[j],hist[j],rel); //rel will have only the contents of bucket j
+				printf("gate 7\n");
+				radix_sort(rel,rel2,(depth+1),psum[j]); //radix_sort only on rel that now has only the contents of bucket j and depth is incr by 1
+				printf("gate 8\n");
+				//copy_relation (rel2,psum[j],hist[j],rel); //the sorted result is then copied back to rel2 in the correct place
+				//printf("gate 9\n");
+			}	
+		}
+	}
+	/*printf("gate 10\n");
+	rel->num_tuples = rel2->num_tuples; //finally rel becomes a sorted.
+	printf("gate 11\n");
+	copy_relation(rel,0,rel2->num_tuples,rel2);*/
+	printf("all clear\n");
+	return 0;
+}
 
 int copy_relation (relation *to,uint64_t start,uint64_t size,relation *from){ //copies contents from relation from to relation to at a specific place
 	uint64_t  i;
@@ -149,10 +166,10 @@ int create_hist (relation *rel,uint64_t* hist,int n){ //creates hist
 	return 0;
 }
 
-int create_psum (uint64_t *hist,uint64_t* psum){ //creates psum
+int create_psum (uint64_t *hist,uint64_t* psum,uint64_t start){ //creates psum
 	//int psum[256];
 	int i;
-	psum[0] = 0;
+	psum[0] = start;
 	for (i = 1; i<256;i++)
 		psum[i] = psum[i-1] + hist[i-1];
 	return 0;
--- relation.h
+++ relation.h
@@ -6,7 +6,15 @@ typedef struct relation {
 	uint64_t num_tuples;
 } relation;
 
-int radix_sort(relation *rel,int depth);
+/*typedef struct hl{
+	uint64_t cont[256];
+	int depth;
+	hist* next;	
+} hl;*/
+
+int sort(relation *rel);
+
+int radix_sort(relation *rel,relation *rel2,int depth,uint64_t start);
 
 int copy_relation (relation *to,uint64_t start,uint64_t size,relation *from);
 
@@ -18,9 +26,11 @@ void swap(tuple *a,tuple *b);
 
 int isolate(relation *rel,uint64_t start,uint64_t size,relation *out);
 
+//int create_hiar (relation *rel,hl *hist,int n);
+
 int create_hist (relation *rel,uint64_t* hist,int n);
 
-int create_psum (uint64_t *hist,uint64_t* psum);
+int create_psum (uint64_t *hist,uint64_t* psum,uint64_t start);
 
 void check_sorting(relation *rel,int length);
 
