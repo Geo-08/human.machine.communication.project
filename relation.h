b2d1dcd328d1789c1392a3f2006a9eae5d90a026
Geo <sdi1200192@di.uoa.gr>
2019-10-30 19:55:24 +0200

    rest of files

---
+++ relation.h
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include "tuple.h"
+
+typedef struct relation {
+	tuple *tuples;
+	uint64_t num_tuples;
+} relation;
+
+int radix_sort(relation *rel,int depth);
+
+int copy_relation (relation *to,uint64_t start,uint64_t size,relation *from);
+
+void quicksort (relation *rel,uint64_t low,uint64_t high);
+
+uint64_t partition(relation *rel,int low,int high);
+
+void swap(tuple *a,tuple *b);
+
+int isolate(relation *rel,uint64_t start,uint64_t size,relation *out);
+
+int create_hist (relation *rel,uint64_t* hist,int n);
+
+int create_psum (uint64_t *hist,uint64_t* psum);
+
+void check_sorting(relation *rel,int length);
+
+void print_relation(relation *rel, int length);
+//int print_hist (int* hist);
---
+++ tuple.h
@@ -0,0 +1,8 @@
+#include <inttypes.h>
+
+typedef struct tuple {
+	uint64_t key;
+	uint64_t payload;
+} tuple;
+
+unsigned int n_first_bytes_num (tuple tup,int n);
