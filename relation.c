02a1074ed72a53c796794a72f369a80a39d876a7
Syringas Stylianos <sdi1600164@di.uoa.gr>
2019-11-02 14:21:27 +0000

    Replace relation.c

--- relation.c
+++ relation.c
@@ -58,6 +58,54 @@ int copy_relation (relation *to,uint64_t start,uint64_t size,relation *from){ //
 	return 0;
 }
 
+relation* createRandomRelation(void) {
+	int i, j ;
+	uint64_t size, random_key, random_payload ;
+	size=rand() ;
+	relation* rel ;
+	rel=malloc(sizeof(relation)) ;
+	rel->tuples=malloc(sizeof(tuple)*size) ;
+	rel->num_tuples=size ;
+	for (i=0 ; i<size ; i++) {
+		random_key=0 ;
+		random_payload=0 ;
+		for (j=0 ; j<64 ; j++) {
+			random_key=random_key*2 + rand()%2 ;
+			random_payload=random_payload*2 + rand()%2 ;
+		}
+		rel->tuples[i].key =  random_key ;
+		rel->tuples[i].payload =  random_payload ;
+	}
+	return rel ;
+}
+
+relation* readRelation(char* fileName) {
+	relation* rel ;
+	uint64_t key, payload ;
+	FILE* file ;
+	file=fopen(fileName, "r") ;
+	char* line ;
+	size_t length ;
+	char* token ;
+	rel=malloc(sizeof(relation)) ;
+	if (file) {
+		while(getline(&line, &length, file)) {
+			token=strtok(line, ", ") ;
+			key=atoi(token) ;
+			printf("%s\n", token) ;
+			token=strtok(NULL, ", ") ;
+			payload=atoi(token) ;
+		}
+	}
+	fclose(file) ;
+	return rel ;
+}
+
+void deleteRelation(relation* rel) {
+	free(rel->tuples) ;
+	free(rel) ;uint64_t payS, uint64_t ;
+}
+
 void quicksort (relation *rel,uint64_t low,uint64_t high){
 	if (low < high){
 		uint64_t q;
