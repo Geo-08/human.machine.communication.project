d3326a57be675283fde3fc1e334f78d20b84c91a
Syringas Stylianos <sdi1600164@di.uoa.gr>
2019-11-02 14:35:25 +0000

    Upload New File

---
+++ sortmj.h
@@ -0,0 +1,19 @@
+#ifndef SORTMJ_H
+#define SORTMJ_H
+
+#include "relation.h"
+#include "string.h"
+#define BUFFSIZE 1024*1024
+
+typedef struct buffer{
+	void *memory;
+	struct buffer *next;
+	unsigned int memleft;
+} buffer;
+
+void createBuffer(buffer **Buff);
+void join(uint64_t payloadR, uint64_t payloadS, buffer *Buff);
+buffer *merge(relation *relR, relation *relS);
+buffer *SortMergeJoin(relation* relR, relation* relS);
+
+#endif
