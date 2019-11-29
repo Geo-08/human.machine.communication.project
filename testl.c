7d0ec5918b9007871ae594484aa6490defcf59ba
Geo <sdi1200192@di.uoa.gr>
2019-11-29 13:43:45 +0200

    Main for testing query_comp()

--- testl.c
+++ testl.c
@@ -2,9 +2,11 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-	query* qu;
-	query_init(&qu);
-	read_query(qu,"0 2 4|0.1=1.2&1.0=2.1&0.1>3000|0.0 1.1");
-	printf("Printing Query\n");
-	print_query(qu);
+	TableStorage* store;
+	store =  readTables();
+	query_comp(store,"3 0 1|0.2=1.0&0.1=2.0&0.2>3499|1.2 0.1");
+	/*query_comp(store,"5 0|0.2=1.0&0.3=9881|1.1 0.2 1.0");
+	query_comp(store,"9 0 2|0.1=1.0&1.0=2.2&0.0>12472|1.0 0.3 0.4");
+	query_comp(store,"9 0|0.1=1.0&0.1>1150|0.3 1.0");*/
+	deleteTableStorage(store);
 }
