31618c25d231c21108252c64177f518d88c2475a
Syringas Stylianos <sdi1600164@di.uoa.gr>
2019-12-14 17:35:41 +0000

    Can print total time, plus free output of query_comp

--- main.c
+++ main.c
@@ -1,21 +1,31 @@
 #include <stdio.h>
 #include <inttypes.h>
 #include <stdlib.h>
+#include <time.h>
 #include "tableStorage.h"
 #include "opperations.h"
 
 /*To run the program for small write "./script small | ./project2*/
 
 int main(int argc, char** argv) {
+	clock_t start, end ;
+	double total_time ;
+	start = clock() ;
+
 	TableStorage* tableStorage ;
 	tableStorage=readTables() ;
 	size_t length=100 ;
 	char* query=malloc(100*sizeof(char)) ;
 	while (getline(&query, &length, stdin)!=-1) {
-		if (strcmp(query, "F\n")!=0)
-			query_comp(tableStorage, query) ;
+		if (strcmp(query, "F\n")!=0) {
+			uint64_t* out=query_comp(tableStorage, query) ;
+			free(out) ;
+		}
 	}	
 	free(query) ;
 	deleteTableStorage(tableStorage) ;
+	end=clock() ;
+	total_time=((double)(end-start))/CLOCKS_PER_SEC ;
+	printf("Total time:%f seconds.\n", total_time) ;
 	return 0 ;
 }
