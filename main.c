658e0324427b74cdcdc36124a4e3b8dead832c14
Geo <sdi1200192@di.uoa.gr>
2020-01-17 13:45:49 +0200

    main now uses optimizer and comp_v2

--- main.c
+++ main.c
@@ -12,17 +12,19 @@ int main(int argc, char** argv) {
 	clock_t start, end ;
 	double total_time ;
 	start = clock() ;
-
+	query* qu;
 	TableStorage* tableStorage ;
 	tableStorage=readTables() ;
 	size_t length=100 ;
 	char* query=malloc(100*sizeof(char)) ;
 	while (getline(&query, &length, stdin)!=-1) {
 		if (strcmp(query, "F\n")!=0) {
-			printf("start\n");
-			opt_query(tableStorage,query);
-			printf("end\n");
-			uint64_t* out=query_comp(tableStorage, query) ;
+			//printf("start\n");
+			qu = opt_query(tableStorage,query);
+			//printf("end\n");
+			//printf("calculating query\n");
+			uint64_t* out=query_comp_v2(tableStorage, qu) ;
+			//uint64_t* out=query_comp(tableStorage, query) ;
 			free(out) ;
 		}
 	}	
