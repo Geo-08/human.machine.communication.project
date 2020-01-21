46ae9727fdb6c1ee5095eedd15674dcb1cbf7247
Syringas Stylianos <sdi1600164@di.uoa.gr>
2020-01-21 04:42:30 +0000

    Sort and Join Jobs

--- job.h
+++ job.h
@@ -5,24 +5,37 @@
 #include <stdlib.h>
 #include <string.h>
 #include <stdio.h>
+#include <semaphore.h>
 #include "opperations.h"
 #include "tableStorage.h"
 
 typedef struct Job {
-	int type ; //0:QueryJob, 1:SortJob
+	int type ; //0:QueryJob, 1:SortJob, 2:JoinJob, 3:RadixSortJob
 	int index ;
 	char* query ;
 	TableStorage* tableStorage ;
+	relation* rel ;
+	relation* rel2 ;
+	relation* result ;
+	sem_t* query_sem ;
+	sem_t* sort_sem ;
+	sem_t* radix_sort_sem ;
+	sem_t* count_sem ;
+	sem_t* join_sem ;
+	pthread_mutex_t* count_mutex ;
+	int* count ;
+	uint64_t start ;
+	uint64_t end ;
 } Job;
 
-typedef struct node {
+typedef struct queue_node {
 	Job* job ;
-	struct node* next ;
-} Node;
+	struct queue_node* next ;
+} QueueNode;
 
 typedef struct JobQueue {
-	Node* first ;
-	Node* last ;
+	QueueNode* first ;
+	QueueNode* last ;
 	int jobCount ;
 } JobQueue;
 
@@ -31,7 +44,13 @@ JobQueue* createJobQueue(void) ;
 void addJob(JobQueue*, Job*) ;
 Job* getJob(JobQueue*) ;
 
-Job* QueryJobInit(int, char*, TableStorage*) ;
+Job* QueryJobInit(int, char*, TableStorage*, sem_t*) ;
+Job* SortJobInit(relation*, sem_t*, pthread_mutex_t*, int*, sem_t*) ;
+Job* RadixSortJobInit(relation*, relation*, sem_t*, uint64_t, uint64_t, sem_t*) ;
+Job* JoinJobInit(relation*, relation*, relation*, sem_t*) ;
 void QueryJobDelete(Job*) ;
+void SortJobDelete(Job*) ;
+void RadixSortJobDelete(Job*) ;
+void JoinJobDelete(Job*) ;
 
 #endif
