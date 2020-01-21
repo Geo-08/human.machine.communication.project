0b5ea72de1b64f45aaad2288fef05374c55855f9
Syringas Stylianos <sdi1600164@di.uoa.gr>
2020-01-21 04:47:24 +0000

    Sort works parallel

--- sortmj.h
+++ sortmj.h
@@ -3,8 +3,13 @@
 
 #include "relation.h"
 #include <string.h>
+#include <semaphore.h>
+#include "jobScheduler.h"
+#include "job.h"
 #define BUFFSIZE 1024*1024
 #define MAXS 65536
+
+typedef struct JobScheduler JobScheduler ;
 
 /*typedef struct buffer{
 	void *memory;
@@ -20,9 +25,11 @@ void printresult(buffer *Buff);
 void deletebuffer(buffer *Buff);
 */
 
-int sort(relation *rel);
+int sort_v2(relation *rel, sem_t* sort_sem, pthread_mutex_t* count_mutex, int* count, JobScheduler* jobScheduler, sem_t* count_sem);
 
 int radix_sort(relation *rel,relation *rel2,int depth,uint64_t start);
+
+int radix_sort_v2(relation *rel,relation *rel2,int depth,uint64_t start,uint64_t end);
 
 void quicksort (relation *rel,uint64_t low,uint64_t high);
 
@@ -32,6 +39,12 @@ void swap(tuple *a,tuple *b);
 
 int create_hist (relation *rel,uint64_t* hist,int n);
 
+int create_hist_v2 (relation *rel,uint64_t* hist,int n,uint64_t start,uint64_t end);
+
 int create_psum (uint64_t *hist,uint64_t* psum,uint64_t start);
+
+int sort(relation *rel);
+
+void getSize (uint64_t* hist, uint64_t* start, uint64_t* end) ;
 
 #endif
